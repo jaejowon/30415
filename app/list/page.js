ecade38a6cd8cb79bd8d907a330f99440187d0a6
jaejowon <joharry0915@naver.com>
2023-08-29 12:49:45 +0900

    230829

---
+++ app/list/page.js
@@ -0,0 +1,11 @@
+export default function List() {
+    return (
+    <div>
+        <center>
+            <h2>다함께 차차차</h2>
+            <h2>설운도</h2>
+            <h2>1991-06-01</h2>
+        </center>
+    </div>
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <center>
+      홈이랑께
+      </center>
+    </div>
+  )
+}
